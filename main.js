1fe5f9d36c44666ac962d14f6d275e78a3b869ba
junior <junior.oliveira05@icloud.com>
2023-09-23 14:09:12 -0300

    adicionando main.js na página de formulario

---
+++ main.js
@@ -0,0 +1,4 @@
+ function clicar () {
+    alert("Obrigado, vamos entra em contato!") 
+    history.go (0);//Método history.go (0): Este método carrega uma URL do histórico do navegador dependendo do parâmetro passado a ele.
+}
