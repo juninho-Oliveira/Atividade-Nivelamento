1d6cedbe8b632acdcf57b26efdaca9e97afbe0a9
junior <junior.oliveira05@icloud.com>
2023-09-23 14:32:53 -0300

    adicionando alert

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
  function clicar () {
-    alert("Obrigado, vamos entra em contato!") 
+
+    let nome = document.querySelector(".nome").value;
+    //Esse IF serve para só chamar o alert se o nome for preenchido (a variável nome tiver valor
+    if (nome) {
+      alert(`Obrigado ${nome},  vamos entra em contato!`);
+    } else {
+        alert("Por favor informe o nome!")
+    }
+     
     history.go (0);//Método history.go (0): Este método carrega uma URL do histórico do navegador dependendo do parâmetro passado a ele.
+   
+
 }
+
