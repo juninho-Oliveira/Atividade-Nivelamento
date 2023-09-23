149e809b7f1b728448d1d9dd23952933823e33be
Damião Junior <123269234+juninho-Oliveira@users.noreply.github.com>
2023-09-23 17:42:16 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
  function clicar () {
 
-    let nome = document.querySelector(".nome").value;
+    let nomes = document.querySelector(".nome").value;
     let email = document.querySelector(".email").value;
     let mensagem = document.querySelector(".mensagem").value;
     //Esse IF serve para só chamar o alert se não for todos os dados preenchidos (a variável nome, email, mensagem tiver valor )
-    if (nome === "" || email === "" || mensagem === "") {
+    if (nomes === "" || email === "" || mensagem === "") {
       alert("Por favor preencha todos os campos!")
     } else {
         alert(`Obrigado ${nome},  vamos entra em contato!`);
